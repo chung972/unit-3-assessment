2deb2bded8f69a793f4c31de52ae197169e9281e
Albert Chung <chung972@gmail.com>
2019-06-21 13:21:24 -0500

    adds class based view functions for wish

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+from .models import Wish
 
 # Create your views here.
+
+
+class WishList(ListView):
+    model = Wish
+
+
+class WishDetail(DetailView):
+    model = Wish
+
+
+class WishCreate(CreateView):
+    model = Wish
+    fields = '__all__'
+
+
+class WishUpdate(UpdateView):
+    model = Wish
+    fields = '__all__'
+
+
+class WishDelete(DeleteView):
+    model = Wish
+    success_url = '/'
