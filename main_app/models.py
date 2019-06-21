93076cf50142663a43d633b75f19d2974edf6577
Albert Chung <chung972@gmail.com>
2019-06-21 13:20:50 -0500

    creates wish model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.shortcuts import render
+from django.urls import reverse
 
 # Create your models here.
+
+
+class Wish(models.Model):
+    description = models.TextField(max_length=500)
+
+    def get_absolute_url(self):
+        return reverse('wish_list')
